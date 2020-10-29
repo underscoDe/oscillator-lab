d816dbc1b606a5addae98e9cd181229764b7c880
underscoDe <developer.awounou@gmail.com>
2020-10-29 10:48:52 +0100

    changed and styled quiz page menu

--- src/components/main/QuizzPage.js
+++ src/components/main/QuizzPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { goToPenduleSimple, goToPenduleElastiqueEquilibre, goToChoice } from '../../actions';
 import { connect } from 'react-redux';
-import { Button } from "@material-ui/core";
+import Hook from '../../resources/Hook.svg';
+import Pendulum from '../../resources/Pendulum.svg';
+import Spring from '../../resources/Spring.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/QuizzPage.scss';
 
@@ -9,16 +11,20 @@ const QuizzPage = ({ goToPenduleSimplePage, goToPenduleElastiquePage, goToChoice
 
   return (
     <div className="quizzPageRoot">
-      <Button color="default" variant="contained" onClick={ ()=> goToChoicePage() }>Retour</Button>
       <div className='menu__buttons'>
           <ul>
+            <li onClick={()=> goToChoicePage()} className='learn__item'>
+              <FontAwesomeIcon icon='home' size='3x' className='liFa' />
+              <span className='cours__text'>Menu</span>
+            </li>
             <li onClick={() => goToPenduleSimplePage()} className='learn__item'>
-              <FontAwesomeIcon icon='book' size='3x' className='liFa' />
-              <span className='cours__text'>Pendule Simple</span>
+              <img width="70px" height="70px" src={Pendulum} className="liFa liFaS" alt="" />
+              <span className='cours__text spanS'>Pendule Simple</span>
             </li>
             <li onClick={() => goToPenduleElastiquePage()} className='quiz__item'>
-              <FontAwesomeIcon icon='brain' size='3x' className='liFa' />
-              <span className='quiz__text'>Pendule Elastique</span>
+
+            <img width="75px" height="75px" src={Hook} className="liFa liFaE" alt="" />
+              <span className='quiz__text spanE'>Pendule Elastique</span>
             </li>
           </ul>
         </div>
